a1b58ca523a17917c03ec57438caeae6364eef87
dinhtrieu <trieunguyencd17tt10@gmail.com>
2020-06-26 16:25:47 +0700

    commit

--- 1771-module/js/1771.js
+++ 1771-module/js/1771.js
@@ -1,13 +1,15 @@
-var swiper = new Swiper('.swiper-container', {
-      slidesPerView: 1,
-      spaceBetween: 30,
-      loop: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
+$(document).ready(function(){
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+});
